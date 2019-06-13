656c48448757ec9dded01d7f83b43042bda101eb
Vlad <vladstpnv54@mail.ru>
2019-06-13 18:23:51 +0700

    Function "computed"

--- src/js/script.js
+++ src/js/script.js
@@ -45,3 +45,22 @@ var app = new Vue({
       deep: true
     }
   },
+
+  computed: {
+    filteredTodos: function () {
+      return filters[this.visibility](this.todos)
+    },
+    remaining: function () {
+      return filters.active(this.todos).length
+    },
+    allDone: {
+      get: function () {
+        return this.remaining === 0
+      },
+      set: function (value) {
+        this.todos.forEach(function (todo) {
+          todo.completed = value
+        })
+      }
+    }
+  },
