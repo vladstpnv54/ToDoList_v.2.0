75ffd02eb92e8761dcac201c8bae07e4616b8e79
Vlad <vladstpnv54@mail.ru>
2019-06-13 18:16:42 +0700

    Add save function

---
+++ src/js/script.js
@@ -0,0 +1,14 @@
+var STORAGE_KEY = 'todos-vuejs-2.0'
+var todoStorage = {
+  fetch: function () {
+    var todos = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]')
+    todos.forEach(function (todo, index) {
+      todo.id = index
+    })
+    todoStorage.uid = todos.length
+    return todos
+  },
+  save: function (todos) {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(todos))
+  }
+}
